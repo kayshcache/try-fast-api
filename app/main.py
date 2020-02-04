a3d042166969fa918269c333fadba32773478518
Staff GVA <GVA@Sach-Australia.local>
2020-02-05 07:40:09 +1100

    up to oauth2 stuff

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 from .routers import users
+import os
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 # API Version Prefix URL for version 1 endpoints
 # Probably not the right way to do this, just stub it for later
 AVP1 = '/api/v1'
 
+MYSQL_CREDENTIALS = json.loads(os.getenv('MYSQL_CREDENTIALS'))
+
 app = FastAPI(
-        title = 'Fast API Try-Out',
+        title = 'Kaysh Cache API v1',
         description = 'A first experiment creating an API with Python\'s FastAPI package.',
         version = '1.0.0',
 )
@@ -17,14 +26,18 @@ app.include_router(
         tags=['users'],
 )
 
+
 @app.get('/')
-async def root():
-    return {'message': 'Hello World'}
+async def root(token: str = Depends(oauth2_scheme)):
+    return {'message': 'Hello World', 'token': token}
+
 
 @app.post('/{message}')
 async def root(message: str = None):
     return message
 
+
 @app.get('/files/{file_path:path}')
 async def read_user_me(file_path: str = None):
     return {'file_path': file_path}
+
